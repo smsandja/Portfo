1ed1b039fd0b87e2abec5aee3f280ba372a5c51e
Sam <smsandja@gmail.com>
2024-12-05 19:34:56 +0000

    code review

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -10,43 +10,51 @@ const Cv = () => {
                             <div className="timeline">
                                 <div className="timeline-wrapper">
                                     <div className="timeline-yr">
-                                        <span>2019</span>
+                                        <span>Freelance</span>
                                     </div>
                                     <div className="timeline-info">
-                                        <h5><span>Project Manager</span><small>Best Studio</small></h5>
-                                        <p>Proin ornare non purus ut rutrum. Nulla facilisi. Aliquam laoreet libero ac pharetra feugiat. Cras ac fermentum nunc, a faucibus nunc.</p>
+                                        <h5><span>Développeur front/fullstack</span><small>Build IT(2023-Aujourd'hui)</small></h5>
+                                        <p>
+                                                <li>Accompagnement à la digitalisation (Analyse de projets et rédactions des cahiers de charges)</li>
+                                                <li>Développement et maintenance de sites web</li>
+                                                <li>Conception de maquettes</li>
+                                        </p>
                                     </div>
                                 </div>
                                 <div className="timeline-wrapper">
                                     <div className="timeline-yr">
-                                        <span>2018</span>
+                                        <span>CDI</span>
                                     </div>
                                     <div className="timeline-info">
-                                        <h5><span>UX Designer</span><small>Digital Ace</small></h5>
-                                        <p>Fusce rutrum augue id orci rhoncus molestie. Nunc auctor dignissim lacus vel iaculis.</p>
+                                        <h5><span>Informaticien - chef section CIPLEV OTI 2</span><small>CIPLEV-MSPC(2020-Aujourd'hui)</small></h5>
+                                        <p>
+                                        <li>Chef section CIPLEV OTI2, responsable de la mise en œuvre du programme de
+                                        prévention et de lutte contre extrémisme violent : Gestion des données, gestion des
+                                        besoins et exigences du CIPLEV auprès des acteurs locaux et partenaires (Plan
+                                        International, OIM, CEJP). </li>
+                                        <li>Mise en place d’une application de gestion et suivi des sensibilisations dans l’OTI </li>
+                                        <li>Mise en place d’une application de gestion des réservations du centre des jeunes de
+                                        Barkoissi pour le compte de la mairie de l’OTI2</li>
+                                        <li>Gestion des données de vaccination contre la Covid 19 pour le compte du CHP de Mango
+                                        </li>
+                                        <li>Support IT</li>
+                                        </p>
                                     </div>
                                 </div>
 
                                 <div className="timeline-wrapper">
                                     <div className="timeline-yr">
-                                        <span>2016</span>
+                                        <span>CDD</span>
                                     </div>
                                     <div className="timeline-info">
-                                        <h5><span>UI Freelancer</span></h5>
-                                        <p>Sed fringilla vitae enim sit amet cursus. Sed cursus dictum tortor quis pharetra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                                        <h5><span>Développeur front-end</span><small>Digiket-Afrik(Mars 2019- Aout 2019)</small></h5>
+                                        <p> 
+                                            <li>Accompagnement à la digitalisation (Analyse de projets et rédactions des cahiers de charges)</li>
+                                            <li>Développement et maintenance de sites web</li>
+                                            <li>Conception de maquettes</li>
+                                        </p>
                                     </div>
                                 </div>
-                                
-                                <div className="timeline-wrapper">
-                                    <div className="timeline-yr">
-                                        <span>2014</span>
-                                    </div>
-                                    <div className="timeline-info">
-                                        <h5><span>Junior Designer</span><small>Crafted Co.</small></h5>
-                                        <p>Cras scelerisque scelerisque condimentum. Nullam at volutpat mi. Nunc auctor ipsum eget magna consequat viverra.</p>
-                                    </div>
-                                </div>
-
                             </div>
                         </div>
 
@@ -56,31 +64,31 @@ const Cv = () => {
                             <div className="timeline">
                                 <div className="timeline-wrapper">
                                     <div className="timeline-yr">
-                                        <span>2017</span>
+                                        <span>2016</span>
                                     </div>
                                     <div className="timeline-info">
-                                <h5><span>Mobile Web</span><small>Master Design</small></h5>
-                                        <p>Please tell your friends about Tooplate website. That would be very helpful. We need your support.</p>
+                                <h5><span>BAC D</span><small>Lycée Moderne le Jourdain"</small></h5>
+                                        <p>Mathématique et science de la nature, </p>
                                     </div>
                                 </div>
 
                                 <div className="timeline-wrapper">
                                     <div className="timeline-yr">
-                                        <span>2015</span>
+                                        <span>2018</span>
                                     </div>
                                     <div className="timeline-info">
-                                        <h5><span>User Interfaces</span><small>Creative Agency</small></h5>
-                                        <p><a rel="nofollow" href="https://www.facebook.com/tooplate">Tooplate</a> is a great website to download HTML templates without any login or email.</p>
+                                        <h5><span>BTS en Informatique de Gestion</span><small>Ecole Superieure des Affaires(ESA)</small></h5>
+                                        <p>Thème de soutanence "Gestion et suivi des stages : Cas de Radio Lomé "</p>
                                     </div>
                                 </div>
                                 
                                 <div className="timeline-wrapper">
                                     <div className="timeline-yr">
-                                        <span>2013</span>
+                                        <span>2019</span>
                                     </div>
                                     <div className="timeline-info">
-                                        <h5><span>Artwork Design</span><small>New Art School</small></h5>
-                                        <p>You can freely use Tooplate's templates for your business or personal sites. You cannot redistribute this template without a permission.</p>
+                                        <h5><span>Licence en Génie Logiciel</span><small>ESA</small></h5>
+                                        <p>Thème de soutanence "Mise en place d'une application de gestion d'une boutique de ventes en ligne : Cas de Balouki Shop "</p>
                                     </div>
                                 </div>
 
