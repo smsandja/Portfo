af0ecc0e8f9aa92b8135b0fab0f1b315a38841c0
Sam <smsandja@gmail.com>
2024-12-05 14:45:19 +0000

    babel package added

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,6 @@ const About = () => {
             <section className="about full-screen d-lg-flex justify-content-center align-items-center" id="about">
                 <div className="container">
                     <div className="row">
-                        
                         <div className="col-lg-7 col-md-12 col-12 d-flex align-items-center">
                             <div className="about-text">
                                 <h5 className="text-info">Bienvenue sur <span className="mobile-block">mon portfolio !</span></h5>
@@ -19,20 +18,17 @@ const About = () => {
                                 </h1>
                                 <p></p><p></p>
                                 <p> Spécialisé dans la création de sites modernes, performants et adaptés à vos besoins, je m'engage à donner vie à vos idées en ligne. Découvrez ici mes projets, compétences et collaborations. Bonne visite !"</p>
-                                
                                 <div className="custom-btn-group mt-4">
                                 <a href="#project" className="btn mr-lg-2 custom-btn"> Projets</a>
                                 <a href="#contact" className="btn custom-btn custom-btn-bg custom-btn-link">Obtenez un devis gratuit</a>
                                 </div>
                             </div>
                         </div>
-
                         <div className="col-lg-5 col-md-12 col-12">
                             <div className="about-image svg">
                                 <img src="images/undraw/vector.jpg" className="img-fluid" alt="svg image" />
                             </div>
                         </div>
-
                     </div>
                 </div>
             </section>
